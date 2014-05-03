e0dd839ccf512a7ffad0bb1d6429db32533bf33e
Al-Safa Tech <Al-Safa Tech@Saleh-Alghamdi>
2014-05-03 17:50:07 +0930

    Initialized test framework (JUnit) for this project and created a test class for Board.

---
+++ test/pkg2048/tests/BoardTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg2048.tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Al-Safa Tech
+ */
+public class BoardTest {
+    
+    public BoardTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
