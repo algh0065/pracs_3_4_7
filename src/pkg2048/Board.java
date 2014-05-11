48bde64c1862cf37d6089570ec898386ca36c4b1
Al-Safa Tech <Al-Safa Tech@Saleh-Alghamdi>
2014-05-11 21:54:20 +0930

    Implemented the basic functions to merge a single column or a single row.

--- src/pkg2048/Board.java
+++ src/pkg2048/Board.java
@@ -32,4 +32,53 @@ public class Board {
     public Board apply(Direction dir) {
         return this;
     }
+
+    private int[] applyRightDown(int[] line, Direction dir) {
+        int i = 0;
+        while (i <= line.length - 3) {
+            int a = line[i];
+            int b = line[i + 1];
+            if (a == b) {
+                line[i + 1] = a + b;
+                line[i] = 0;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        int result[] = new int[line.length];
+        i = line.length - 1;
+        for (int j = line.length - 1; j >= 0; j--) {
+            if (line[j] != 0) {
+                result[i] = line[j];
+                i--;
+            }
+        }
+        return result;
+    }
+
+    private int[] applyLeftUp(int[] line, Direction dir) {
+        int i = line.length - 1;
+        while (i >= 1) {
+            int a = line[i];
+            int b = line[i - 1];
+            if (a == b) {
+                line[i - 1] = a + b;
+                line[i] = 0;
+                i -= 2;
+            } else {
+                i--;
+            }
+        }
+        int result[] = new int[line.length];
+        i = 0;
+        for (int j = 0; j < line.length; j++) {
+            if (line[j] != 0) {
+                result[i] = line[j];
+                i++;
+            }
+        }
+        return result;
+    }
+
 }
