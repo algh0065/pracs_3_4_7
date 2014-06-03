a701cea6fde7f67d7a6e623a345beaac70f602f4
Al-Safa Tech <Al-Safa Tech@Saleh-Alghamdi>
2014-06-03 22:26:30 +0930

    I implemented a Function to read the next move from standard input.

--- src/pkg2048/Main.java
+++ src/pkg2048/Main.java
@@ -6,6 +6,8 @@
 
 package pkg2048;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Al-Safa Tech
@@ -23,6 +25,20 @@ public class Main {
         Board b = new Board(4, 4);
         b.setBoard(new int[][]{{0, 0, 0, 0}, {2, 0, 0, 0}, {0, 4, 0, 0}, {0, 0, 0, 0}});
         return b;
+    }
+    
+    private Direction getNextMove(Scanner s){
+        switch(s.nextInt()){
+            case 1:
+                return Direction.UP;
+            case 2:
+                return Direction.DOWN;
+            case 3:
+                return Direction.RIGHT;
+            case 4:
+                return Direction.LEFT;
+        }
+        return null;
     }
     
 }
